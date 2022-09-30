80a7ac63658e4d53628790b942dde781f90bcd88
Johan <johanclement88@hotmail.fr>
2022-09-30 21:59:10 +0200

    Création du projet, ajout des librairies, création route homepage et dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route } from 'wouter';
+import Homepage from './Homepage';
+import Dashboard from './Dashboard';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <Route path="/" component={Homepage} />
+            <Route path="/dashboard" component={Dashboard} />
+        </>
+    );
 }
 
 export default App;
